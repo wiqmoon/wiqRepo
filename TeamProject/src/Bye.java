04325fb69897b7e0432b3ce51c2b4cda7160c83d
Hanbin <wiqdev@naver.com>
2023-02-04 01:07:17 +0900

    Update Bye.java

--- TeamProject/src/Bye.java
+++ TeamProject/src/Bye.java
@@ -4,6 +4,7 @@ public class Bye {
 	public static void main(String[] args) {
 		System.out.println("안녕히 가세요.");
 		System.out.println("Bye, see you again");
+		System.out.println("Zai jian!");
 	}
 
 }
