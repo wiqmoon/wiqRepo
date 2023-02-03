5efb1b977e32fecb06f77017fd3eac0a350bc25b
wiqmoon <wiqdev@naver.com>
2023-02-04 00:30:09 +0900

    first commit

---
+++ src/myRepository/Hello.java
@@ -0,0 +1,10 @@
+package myRepository;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요.");
+		System.out.println("Hello, nice to meet you.");
+	}
+
+}
