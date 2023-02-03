7ae3d5fa2b11125317398ef70327c7da4bb55654
wiqmoon <wiqdev@naver.com>
2023-02-04 00:50:46 +0900

    Update 2 Hello.java

--- TeamProject/src/Hello.java
+++ TeamProject/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요.");
 		System.out.println("Hello, nice to meet you.");
-		System.out.println("Ni hao. Jian nin hengaoxing.")
+		System.out.println("Ni hao. Jian nin hengaoxing.");
 	}
 
 }
